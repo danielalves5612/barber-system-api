a268fbbc3a5537f56e0c6d77221917c5b8b073d6
daniel5612 <da442494@gmail.com>
2026-07-08 14:02:43 -0300

    Ajusta configuração para deploy

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import app from './app.js';
 
-const port = 3001
+const port = process.env.APP_PORT || process.env.PORT || 3001
 
 app.listen(port, () => {
-    console.log('Servidor rodando na porta ' + port);
+    console.log(`Servidor rodando na porta ${port}`);
     console.log('Acesse http://localhost:3001')
 })
