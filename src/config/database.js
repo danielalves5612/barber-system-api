82febe8b3060365d30ea9c8be128bde62f6f130e
daniel5612 <da442494@gmail.com>
2026-07-08 14:46:18 -0300

    Ajusta o username do banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,7 @@ const configDatabase = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_DATABASE,
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     define: {
         timestamps: true,
