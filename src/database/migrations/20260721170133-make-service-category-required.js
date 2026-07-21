531494d79fbea253abec4df35e7f87fa6d69fbbf
daniel5612 <da442494@gmail.com>
2026-07-21 14:08:57 -0300

    Torna a categoria dos serviços obrigatória

---
+++ src/database/migrations/20260721170133-make-service-category-required.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up (queryInterface, Sequelize) {
+
+    await queryInterface.changeColumn('services', 'categoria', 
+      { 
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    );
+  
+  },
+
+  async down (queryInterface, Sequelize) {
+
+    await queryInterface.changeColumn('services', 'categoria', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
+
+  }
+};
